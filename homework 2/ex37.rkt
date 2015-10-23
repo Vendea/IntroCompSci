77245df3f692d36f120b68ce81535a89b00bb8b4
Vendea <kbeine@live.com>
2015-10-23 09:26:41 -0500

    portfolio 2 done

---
+++ homework 2/ex37.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex37) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+; exercise 37
+; string -> character
+(define (string-last s) 
+  (substring  s (- (string-length s) 1) (string-length s)))
+(check-expect (string-last "abcdefg") "g")
